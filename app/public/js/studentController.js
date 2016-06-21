4c1276872a3e72c7b06d46ea9411e3812f464b0f
adi2233 <adibar_1@walla.co.il>
2016-06-21 13:30:17 +0300

    Update studentController.js

--- app/public/js/studentController.js
+++ app/public/js/studentController.js
@@ -1,7 +1,7 @@
 var showStudent = angular.module('showStudent',[]);
 
 showStudent.controller('showStud',['$scope','$http',function($scope,$http) {
-     $http.get("http://localhost:3000/students").success(function(data){
+     $http.get("https://hw2-2-server.herokuapp.com/students").success(function(data){
         $scope.studentController = data; 
         console.log(data);
     });
